5f897ca694e22ab2a10258ffc356aca3510a41f8
Yevgeni Shapiro <23049337+crypterio-yevgeni@users.noreply.github.com>
2025-06-20 23:37:37 +0300

    update

--- install-k3s.sh
+++ install-k3s.sh
@@ -5,14 +5,17 @@ MASTER_IP=$2
 MASTER_COUNT=$3
 
 if [ "$NODE_INDEX" -eq 0 ]; then
-  # First master initializes cluster
+  echo "[+] Installing K3s as first master..."
   curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --write-kubeconfig-mode 644" sh -
 else
+  echo "[+] Fetching K3s token from primary master..."
+  TOKEN=$(ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa ubuntu@$MASTER_IP "sudo cat /var/lib/rancher/k3s/server/node-token")
+  
   if [ "$NODE_INDEX" -lt "$MASTER_COUNT" ]; then
-    # Other masters join cluster
-    curl -sfL https://get.k3s.io | K3S_URL="https://${MASTER_IP}:6443" K3S_TOKEN=$(cat /var/lib/rancher/k3s/server/node-token) INSTALL_K3S_EXEC="server" sh -
+    echo "[+] Joining as additional master..."
+    curl -sfL https://get.k3s.io | K3S_URL="https://$MASTER_IP:6443" K3S_TOKEN="$TOKEN" INSTALL_K3S_EXEC="server" sh -
   else
-    # Workers join
-    curl -sfL https://get.k3s.io | K3S_URL="https://${MASTER_IP}:6443" K3S_TOKEN=$(cat /var/lib/rancher/k3s/server/node-token) sh -
+    echo "[+] Joining as worker..."
+    curl -sfL https://get.k3s.io | K3S_URL="https://$MASTER_IP:6443" K3S_TOKEN="$TOKEN" sh -
   fi
 fi
