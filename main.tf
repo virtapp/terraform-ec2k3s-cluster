8bea9622d5fefd0a63830bb6c00493d2b7e88a8a
Yevgeni Shapiro <23049337+crypterio-yevgeni@users.noreply.github.com>
2025-06-20 23:49:34 +0300

    update

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "aws_instance" "k3s_nodes" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = aws_key_pair.generated_key.key_name
-  subnet_id     = aws_subnet.default.id
+  subnet_id     = data.aws_subnet.default.id
   vpc_security_group_ids = [aws_security_group.k3s_sg.id]
   associate_public_ip_address = true
 
