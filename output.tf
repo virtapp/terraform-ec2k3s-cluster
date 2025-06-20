8be2ee46f3fcda0918ff88855af14e4d828e3abc
Yevgeni Shapiro <23049337+crypterio-yevgeni@users.noreply.github.com>
2025-06-21 01:46:23 +0300

    update

--- output.tf
+++ output.tf
@@ -1,4 +1,19 @@
+output "k3s_master_ips" {
+  description = "Public IPs of all master nodes"
+  value       = aws_instance.k3s_master[*].public_ip
+}
 
-output "k3s_master_ip" {
-  value = element(aws_instance.k3s_nodes.*.public_ip, 0)
+output "k3s_worker_ips" {
+  description = "Public IPs of all worker nodes"
+  value       = aws_instance.k3s_worker[*].public_ip
+}
+
+output "k3s_master_private_ips" {
+  description = "Private IPs of all master nodes"
+  value       = aws_instance.k3s_master[*].private_ip
+}
+
+output "k3s_worker_private_ips" {
+  description = "Private IPs of all worker nodes"
+  value       = aws_instance.k3s_worker[*].private_ip
 }
